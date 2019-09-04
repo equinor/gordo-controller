84b9d8f278399f415daef51a080476b41482d58d
Miles Granger <miles59923@gmail.com>
2019-09-04 13:39:56 +0200

    Add kubernetes testing
    Additionally move minor_version test into tests module

---
+++ src/tests/helpers.rs
@@ -0,0 +1,45 @@
+use kube::api::{DeleteParams, ListParams};
+use kube::{api::Api, client::APIClient, config};
+use serde_json::Value;
+use serde_yaml;
+
+use crate::Gordo;
+
+// Get the `APIClient` using current kube config
+pub fn client() -> APIClient {
+    let config = config::load_kube_config().unwrap_or_else(|_| {
+        config::incluster_config().expect("Failed to get local kube config and incluster config")
+    });
+    APIClient::new(config)
+}
+
+// Get an API to the Gordo custom resource
+pub fn gordo_custom_resource_api(client: APIClient) -> Api<Gordo> {
+    let gordos: Api<Gordo> = Api::customResource(client, "gordos")
+        .version("v1")
+        .group("equinor.com")
+        .within("default");
+    gordos
+}
+
+// Remove _all_ gordos.
+pub fn remove_gordos(gordos: &Api<Gordo>) {
+    gordos
+        .list(&ListParams::default())
+        .unwrap()
+        .items
+        .iter()
+        .for_each(|gordo| {
+            gordos
+                .delete(&gordo.metadata.name, &DeleteParams::default())
+                .unwrap();
+        });
+}
+
+// Get the repo's example `Gordo` config file
+pub fn gordo_example_config() -> Value {
+    let config_str =
+        std::fs::read_to_string(format!("{}/example-gordo.yaml", env!("CARGO_MANIFEST_DIR")))
+            .expect("Failed to read config file");
+    serde_yaml::from_str(&config_str).expect("Unable to parse config file into yaml")
+}
---
+++ src/tests/mod.rs
@@ -0,0 +1,3 @@
+pub(crate) mod helpers;
+#[cfg(test)]
+pub(crate) mod test_controller;
