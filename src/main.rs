e35a3816b36f97c293f61547ea050b9116fb6a41
Miles Granger <miles59923@gmail.com>
2020-01-02 13:03:34 +0100

    Use Actix provided async runtime

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use actix_web::{middleware, web, App, HttpServer};
 use gordo_controller::{controller_init, views, GordoEnvironmentConfig};
 use kube::config;
 use log::info;
-use std::sync::mpsc;
 
-#[tokio::main]
+#[actix_rt::main]
 async fn main() -> () {
     std::env::set_var("RUST_LOG", "info,kube=info");
     env_logger::init();
@@ -20,32 +19,20 @@ async fn main() -> () {
 
     let controller = controller_init(kube_config, env_config).await.unwrap();
 
-    let (sender, receiver) = mpsc::channel();
-
-    // Launch in new thread b/c HttpServer starts own async executor
-    let handle = std::thread::spawn(move || {
-        let sys = actix_rt::System::new("gordo-controller-server");
-
-        let srv = HttpServer::new(move || {
-            App::new()
-                .data(controller.clone())
-                .wrap(middleware::Logger::default().exclude("/health"))
-                .wrap(middleware::Compress::default())
-                .service(web::resource("/health").to(views::health))
-                .service(web::resource("/gordos").to(views::gordos))
-                .service(web::resource("/gordos/{name}").to(views::get_gordo))
-                .service(web::resource("/models").to(views::models))
-                .service(web::resource("/models/{gordo_name}").to(views::models_by_gordo))
-        })
-        .bind(&bind_address)
-        .expect(&format!("Could not bind to '{}'", &bind_address))
-        .run();
-
-        sender.send(srv).unwrap();
-
-        sys.run().unwrap()
-    });
-
-    let _srv = receiver.recv().unwrap();
-    handle.join().unwrap()
+    HttpServer::new(move || {
+        App::new()
+            .data(controller.clone())
+            .wrap(middleware::Logger::default().exclude("/health"))
+            .wrap(middleware::Compress::default())
+            .service(web::resource("/health").to(views::health))
+            .service(web::resource("/gordos").to(views::gordos))
+            .service(web::resource("/gordos/{name}").to(views::get_gordo))
+            .service(web::resource("/models").to(views::models))
+            .service(web::resource("/models/{gordo_name}").to(views::models_by_gordo))
+    })
+    .bind(&bind_address)
+    .expect(&format!("Could not bind to '{}'", &bind_address))
+    .run()
+    .await
+    .unwrap()
 }
