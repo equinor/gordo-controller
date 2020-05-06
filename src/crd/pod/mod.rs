4541ee161a156a0c6703c696523b1337b63b7296
Serhii Koropets <skoropets@gmail.com>
2020-05-06 19:01:49 +0300

    Refactor monitor_pods() fucntion

--- src/crd/pod/mod.rs
+++ src/crd/pod/mod.rs
@@ -19,7 +19,7 @@ pub const POD_MATCH_LABELS: &'static [&'static str] = &[
     "applications.gordo.equinor.com/model-name"
 ];
 
-async fn update_model_status(model_resource: &Api<Model>, model: &Model, new_status: ModelStatus) -> () {
+async fn update_model_status(model_resource: &Api<Model>, model: &Model, new_status: ModelStatus) {
     match patch_model_status(model_resource, model, new_status).await {
         Ok(new_model) => info!("Patching Model '{}' from status {:?} to {:?}", model.metadata.name, model.status, new_model.status),
         Err(err) => error!( "Failed to patch status of Model '{}' - error: {:?}", model.metadata.name, err),
@@ -27,44 +27,76 @@ async fn update_model_status(model_resource: &Api<Model>, model: &Model, new_sta
 }
 
 pub async fn monitor_pods(controller: &Controller) -> () {
-    let pods = controller.pod_state().await;
-    //TODO to perform the pod-models matching in O(1) makes sense to do collect into some sort of HashMap here
-    let running_pods: Vec<&Object<PodSpec, PodStatus>> = pods.iter()
-        .filter(|pod| match pod.status.as_ref().and_then(|status| status.phase.as_ref()) {
-                Some(phase) => phase == RUNNING || phase == PENDING,
-                None => false
-        }).collect();
-    if running_pods.is_empty() {
+    let models = controller.model_state().await;
+    let actual_models: Vec<_> = models.into_iter()
+        .filter(|model| match &model.status {
+            Some(status) => status.phase == ModelPhase::Unknown || status.phase == ModelPhase::InProgress,
+            None => true,
+        })
+        .collect();
+    if actual_models.is_empty() {
         return
     }
 
-    debug!("Found {} pods ins state {} or {}", running_pods.len(), RUNNING, PENDING);
-    let models = controller.model_state().await;
-    let status_patchers = models.iter()
-        .flat_map(move |model| match &model.status {
+    let pods = controller.pod_state().await;
+
+    //TODO to perform the models-pods matching in O(1) makes sense to do collect into some sort of HashMap here
+    let actual_pods_labels: Vec<_> = pods.into_iter()
+        .filter(|pod| pod.metadata.labels.get("applications.gordo.equinor.com/model-name").is_some())
+        .flat_map(|pod| match pod.status {
+            Some(status) => match status.phase {
+                Some(phase) => {
+                    if phase == RUNNING || phase == SUCCEEDED {
+                        Some((phase, pod.metadata.labels))
+                    } else {
+                        None
+                    }
+                },
+                _ => None
+            }
+            _ => None
+        })
+        .collect();
+
+    for model in actual_models {
+        let new_model_status = match &model.status {
             Some(status) => {
-                if running_pods.iter()
-                    .any(move |pod| {
+                let pods_labels = &actual_pods_labels;
+                let pods_phases: Vec<_> = pods_labels.into_iter()
+                    .filter(|(_, labels)| {
                         let model_labels = &model.metadata.labels;
-                        let pod_labels = &pod.metadata.labels;
                         POD_MATCH_LABELS.
                             iter().
-                            all(move |&label_name| model_labels.get(label_name) == pod_labels.get(label_name))
-                }) {
+                            all(|&label_name| model_labels.get(label_name) == labels.get(label_name))
+                    })
+                    .map(|(phase, _)| phase)
+                    .collect();
+                if pods_phases.len() > 0 {
                     let mut new_status = status.clone();
-                    new_status.phase = ModelPhase::InProgress;
-                    Some((new_status, model))
+                    let mut new_phase = new_status.phase.clone();
+                    if pods_phases.iter().all(|phase| *phase == SUCCEEDED) {
+                        new_phase = ModelPhase::BuildSucceeded;
+                    } else if pods_phases.iter().any(|phase| *phase == RUNNING) {
+                        new_phase = ModelPhase::InProgress;
+                    }
+                    if new_phase != new_status.phase {
+                        new_status.phase = new_phase;
+                        Some(new_status)
+                    } else {
+                        None
+                    }
                 } else {
                     None
                 }
             },
-            None => Some((ModelStatus::default(), model)),
-        }).map(|(new_status, model)| {
+            None => Some(ModelStatus::default()),
+        };
+        if let Some(new_status) = new_model_status {
             update_model_status(
                 &controller.model_resource,
-                model,
-                new_status.clone(),
-            )
-        });
-    join_all(status_patchers).await;
+                &model,
+                new_status,
+            ).await;
+        }
+    }
 }
